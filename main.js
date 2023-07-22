34ae6fc666186084ea7a4b940fb05c754c4d243f
Furkan Maril <furkan.maril.git@gmail.com>
2023-07-23 01:22:11 +0300

    kodluyoruz-proje18 form exercise completed

---
+++ main.js
@@ -0,0 +1,43 @@
+let formDOM = document.querySelector('#userForm');
+formDOM.addEventListener('submit', formHandler);
+const alertDOM = document.querySelector('#alert');
+const alertFunction = (title, message, className="warning") => `
+    <div class="alert alert-${className} alert-dismissible fade show" role="alert">
+        <strong>${title} </strong>${message}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+`;
+
+function formHandler(event){
+    event.preventDefault();
+    const USER_NAME = document.querySelector('#username');
+    const SCORE = document.querySelector('#score');
+    if(USER_NAME.value && SCORE.value){
+        addItem(USER_NAME.value, SCORE.value);
+        USER_NAME.value = "";
+        SCORE.value = "";
+    }
+    else{
+        // console.log("Hatalı giriş");
+        alertDOM.innerHTML = alertFunction(
+            "Baslik Bilgisi",
+            "Eksik bilgi girdiniz",
+            "danger"
+        );
+    }
+}
+
+let userListDOM = document.querySelector('#userList');
+
+const addItem = (userName, score) => {
+    let liDOM = document.createElement('li');
+    liDOM.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center');
+    liDOM.innerHTML = `
+        ${userName} 
+        <span class="badge badge-primary badge-pill">${score}</span>
+    `;
+    userListDOM.append(liDOM);
+}
+
